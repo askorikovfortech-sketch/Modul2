d219c1e40f04370a854053de5cd36264f2150c9a
artem/s <a.skorikov.fortech@gmail.com>
2026-02-01 18:01:33 +0300

    fix: calling an error, ridden of overwriting keys

--- project/src/components/ExpenseForm/index.jsx
+++ project/src/components/ExpenseForm/index.jsx
@@ -1,97 +1,101 @@
-import { useState } from "react";
-import "./styles.scss";
+  import { useState } from "react";
+  import "./styles.scss";
 
-export const ExpenseForm = ({ addExpense }) => {
-  const [category, setCategory] = useState("");
+  export const ExpenseForm = ({ addExpense }) => {
+    const newExpenses = {category: "", price: ""};
 
-  const [price, setPrice] = useState("");
+    const [expenseForm, setExpenseForm] = useState(newExpenses)
 
-  const [errors, setErrors] = useState({ category: "", price: "" });
+    const [errors, setErrors] = useState(newExpenses);
 
-  const generateDate = () => {
-    const now = new Date();
-    const day = String(now.getDate()).padStart(2, "0");
-    const month = String(now.getMonth() + 1).padStart(2, "0");
-    const year = now.getFullYear();
-    return `${day}.${month}.${year}`;
-  };
-
-  const addClick = () => {
-    setErrors({ category: "", price: "" });
-
-    let hasError = false;
-
-    const newErrors = { category: "", price: "" };
-
-    if (!category.trim()) {
-      newErrors.category = "Поле не должно быть пустым и меньше или равно 0";
-      hasError = true;
-    }
-    if (!price.trim()) {
-      newErrors.price = "Поле не должно быть пустым и меньше или равно 0";
-      hasError = true;
-    }
-    if (Number(price) <= 0) {
-      newErrors.price = "Поле не должно быть пустым и меньше или равно 0";
-      hasError = true;
-    }
-    if (hasError) {
-      setErrors(newErrors);
-      return;
-    }
-
-    const newExpense = {
-      id: Date.now(),
-      category: category.trim(),
-      date: generateDate(),
-      price: price.trim(),
+    const generateDate = () => {
+      const now = new Date();
+      const day = String(now.getDate()).padStart(2, "0");
+      const month = String(now.getMonth() + 1).padStart(2, "0");
+      const year = now.getFullYear();
+      return `${day}.${month}.${year}`;
     };
 
-    addExpense(newExpense);
-    setCategory("");
-    setPrice("");
-  };
+    const addClick = () => {
+      setErrors(newExpenses);
 
-  return (
-    <div className="expense-form">
-      <div className="expense-form__record">
-        <div className="expense-form__record-block">
-          <input
-            type="text"
-            placeholder="Статья расходов"
-            className="expense-form__record-text"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-          <hr className="expense-form__record-line" />
-          {errors.category && (
-            <span className="expense-form__error-message">
-              {errors.category}
-            </span>
-          )}
-        </div>
-        <div className="expense-form__record-block">
-          <input
-            type="number"
-            placeholder="Сумма"
-            className="expense-form__record-text"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            min={0}
-          />
-          <hr className="expense-form__record-line" />
-          {errors.price && (
+      const newErrors = newExpenses;
+
+      if (!expenseForm.category.trim()) {
+        newErrors.category = "Поле не должно быть пустым и меньше или равно 0";
+        setErrors(newErrors);
+        return;
+      }
+      if (!expenseForm.price.trim()) {
+        newErrors.price = "Поле не должно быть пустым и меньше или равно 0";
+        setErrors(newErrors);
+        return;
+      }
+      if (Number(expenseForm.price) <= 0) {
+        newErrors.price = "Поле не должно быть пустым и меньше или равно 0";
+        setErrors(newErrors);
+        return;
+      }
+
+      const createExpense = {
+        id: Date.now(),
+        category: expenseForm.category.trim(),
+        date: generateDate(),
+        price: expenseForm.price.trim(),
+      };
+
+      addExpense(createExpense);
+      setExpenseForm(newExpenses);
+    };
+
+    const processСhange = (box, value) => {
+      setExpenseForm((prev) => ({
+        ...prev,
+        [box]: value,
+      }));
+
+      if (errors[box]) {
+        setErrors((prev) => ({
+          ...prev,
+          [box]: "",
+        }));
+      }
+    };
+
+    return (
+      <div className="expense-form">
+        <div className="expense-form__record">
+          <div className="expense-form__record-block">
+            <input
+              type="text"
+              placeholder="Статья расходов"
+              className="expense-form__record-text"
+              value={expenseForm.category}
+              onChange={(e) => processСhange("category", e.target.value)}
+            />
+            <hr className="expense-form__record-line" />
+            <span className="expense-form__error-message">{errors.category}</span>
+          </div>
+          <div className="expense-form__record-block">
+            <input
+              type="number"
+              placeholder="Сумма"
+              className="expense-form__record-text"
+              value={expenseForm.price}
+              onChange={(e) => processСhange("price", e.target.value)}
+              min={0}
+            />
+            <hr className="expense-form__record-line" />
             <span className="expense-form__error-message">{errors.price}</span>
-          )}
+          </div>
+          <button
+            type="button"
+            className="expense-form__record-button"
+            onClick={addClick}
+          >
+            Добавить
+          </button>
         </div>
-        <button
-          type="button"
-          className="expense-form__record-button"
-          onClick={addClick}
-        >
-          Добавить
-        </button>
       </div>
-    </div>
-  );
-};
+    );
+  };
