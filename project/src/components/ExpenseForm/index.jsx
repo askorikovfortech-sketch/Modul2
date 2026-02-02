9e4445e70ae4fe3bc6844de1a2fb1746cfc30220
ArtemSkor <a.skorikov.fortech@gmail.com>
2026-02-02 17:19:56 +0300

    fix: moved to common components, rid of rewriting the object

--- project/src/components/ExpenseForm/index.jsx
+++ project/src/components/ExpenseForm/index.jsx
@@ -1,101 +1,45 @@
-  import { useState } from "react";
-  import "./styles.scss";
+import "./styles.scss";
 
-  export const ExpenseForm = ({ addExpense }) => {
-    const newExpenses = {category: "", price: ""};
-
-    const [expenseForm, setExpenseForm] = useState(newExpenses)
-
-    const [errors, setErrors] = useState(newExpenses);
-
-    const generateDate = () => {
-      const now = new Date();
-      const day = String(now.getDate()).padStart(2, "0");
-      const month = String(now.getMonth() + 1).padStart(2, "0");
-      const year = now.getFullYear();
-      return `${day}.${month}.${year}`;
-    };
-
-    const addClick = () => {
-      setErrors(newExpenses);
-
-      const newErrors = newExpenses;
-
-      if (!expenseForm.category.trim()) {
-        newErrors.category = "Поле не должно быть пустым и меньше или равно 0";
-        setErrors(newErrors);
-        return;
-      }
-      if (!expenseForm.price.trim()) {
-        newErrors.price = "Поле не должно быть пустым и меньше или равно 0";
-        setErrors(newErrors);
-        return;
-      }
-      if (Number(expenseForm.price) <= 0) {
-        newErrors.price = "Поле не должно быть пустым и меньше или равно 0";
-        setErrors(newErrors);
-        return;
-      }
-
-      const createExpense = {
-        id: Date.now(),
-        category: expenseForm.category.trim(),
-        date: generateDate(),
-        price: expenseForm.price.trim(),
-      };
-
-      addExpense(createExpense);
-      setExpenseForm(newExpenses);
-    };
-
-    const processСhange = (box, value) => {
-      setExpenseForm((prev) => ({
-        ...prev,
-        [box]: value,
-      }));
-
-      if (errors[box]) {
-        setErrors((prev) => ({
-          ...prev,
-          [box]: "",
-        }));
-      }
-    };
-
-    return (
-      <div className="expense-form">
-        <div className="expense-form__record">
-          <div className="expense-form__record-block">
-            <input
-              type="text"
-              placeholder="Статья расходов"
-              className="expense-form__record-text"
-              value={expenseForm.category}
-              onChange={(e) => processСhange("category", e.target.value)}
-            />
-            <hr className="expense-form__record-line" />
-            <span className="expense-form__error-message">{errors.category}</span>
-          </div>
-          <div className="expense-form__record-block">
-            <input
-              type="number"
-              placeholder="Сумма"
-              className="expense-form__record-text"
-              value={expenseForm.price}
-              onChange={(e) => processСhange("price", e.target.value)}
-              min={0}
-            />
-            <hr className="expense-form__record-line" />
-            <span className="expense-form__error-message">{errors.price}</span>
-          </div>
-          <button
-            type="button"
-            className="expense-form__record-button"
-            onClick={addClick}
-          >
-            Добавить
-          </button>
+export const ExpenseForm = ({
+  expenseForm,
+  errors,
+  onUpdsteExpenseForm,
+  onAddExpense,
+}) => {
+  return (
+    <div className="expense-form">
+      <div className="expense-form__record">
+        <div className="expense-form__record-block">
+          <input
+            type="text"
+            placeholder="Статья расходов"
+            className="expense-form__record-text"
+            value={expenseForm.category}
+            onChange={(e) => onUpdsteExpenseForm("category", e.target.value)}
+          />
+          <hr className="expense-form__record-line" />
+          <span className="expense-form__error-message">{errors.category}</span>
         </div>
+        <div className="expense-form__record-block">
+          <input
+            type="number"
+            placeholder="Сумма"
+            className="expense-form__record-text"
+            value={expenseForm.price}
+            onChange={(e) => onUpdsteExpenseForm("price", e.target.value)}
+            min={0}
+          />
+          <hr className="expense-form__record-line" />
+          <span className="expense-form__error-message">{errors.price}</span>
+        </div>
+        <button
+          type="button"
+          className="expense-form__record-button"
+          onClick={onAddExpense}
+        >
+          Добавить
+        </button>
       </div>
-    );
-  };
+    </div>
+  );
+};
--- project/src/pages/HomePages/index.jsx
+++ project/src/pages/HomePages/index.jsx
@@ -7,8 +7,57 @@ import "./styles.scss";
 export const HomePages = () => {
   const [expenses, setExpenses] = useState([]);
 
+  const [expenseForm, setExpenseForm] = useState({ category: "", price: "" });
+
+  const [errors, setErrors] = useState({ category: "", price: "" });
+
+  const generateDate = () => {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = now.getFullYear();
+    return `${day}.${month}.${year}`;
+  };
+
   const addExpense = (newExpense) => {
     setExpenses([...expenses, newExpense]);
+  };
+
+  const updateExpenseForm = (field, value) => {
+    setExpenseForm((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
+  const handleExpense = () => {
+    const errorText = "Поле не должно быть пустым и меньше или равно 0";
+
+    const newErrors = {
+      category: !expenseForm.category.trim() ? errorText : "",
+      price:
+        !expenseForm.price.trim() || Number(expenseForm.price) <= 0
+          ? errorText
+          : "",
+    };
+
+    if (newErrors.category || newErrors.price) {
+      setErrors(newErrors);
+      return;
+    }
+
+    const createExpense = {
+      id: Date.now(),
+      category: expenseForm.category.trim(),
+      date: generateDate(),
+      price: expenseForm.price.trim(),
+    };
+
+    addExpense(createExpense);
+
+    setErrors(newErrors);
+
+    setExpenseForm({ category: "", price: "" });
   };
 
   return (
@@ -16,7 +65,12 @@ export const HomePages = () => {
       <Header />
 
       <main className="main">
-        <ExpenseForm addExpense={addExpense} />
+        <ExpenseForm
+          expenseForm={expenseForm}
+          errors={errors}
+          onUpdsteExpenseForm={updateExpenseForm}
+          onAddExpense={handleExpense}
+        />
         <ExpenseList expenses={expenses} />
       </main>
     </div>
