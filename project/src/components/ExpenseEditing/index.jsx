b4009736a85efc9e3a01d3f106428d90c6fe168a
artem/s <a.skorikov.fortech@gmail.com>
2026-02-03 11:21:17 +0300

    feat: osnova

---
+++ project/src/components/ExpenseEditing/index.jsx
@@ -0,0 +1,32 @@
+import bascet from "../../images/basket.png";
+import check_mark from "../../images/check_mark.png";
+import "./styles.scss";
+export const ExpenseEditing = () => {
+  return (
+    <div className="expense-editing">
+      <div className="expense-editing__block">
+        <input type="text" className="expense-editing__category" />
+        <hr className="expense-editing__line" />
+        <span className="expense-editing__error"></span>
+      </div>
+      <div className="expense-editing__block">
+        <input type="number" className="expense-editing__date" />
+        <hr className="expense-editing__line" />
+        <span className="expense-editing__error"></span>
+      </div>
+      <div className="expense-editing__price">
+        <input type="number" className="expense-editing__price" />
+        <hr className="expense-editing__line" />
+        <span className="expense-editing__error"></span>
+      </div>
+      <div className="expense-editing__button">
+        <button className="verifed">
+          <img src={check_mark} alt="check_mark" />
+        </button>
+        <button className="defer">
+          <img src={bascet} alt="bascet" />
+        </button>
+      </div>
+    </div>
+  );
+};
